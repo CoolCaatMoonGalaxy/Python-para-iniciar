78ea5e5f913cd6b2a06cab376b0d78296d362a61
CoolCatMoon <coronado.sahid@gmail.com>
2024-09-14 22:15:38 -0600

    funciones_basicas.py

---
+++ funciones_basicas.py
@@ -0,0 +1,13 @@
+# <-- este es un comentario de python
+
+variable = "esta es una variable"
+
+# esta es una función en python
+def mostrar_contenido(variable):  # dentro de la función está el parámetro "variable"
+    """ Su contenido debe ir indentado,
+    comúnmente por 4 espacios """
+    return print(variable)  # nos devuelve el valor dentro de "variable"
+
+""" Esta es una llamada a la función.
+    Dentro del paréntesis está el argumento que pasamos a la función """
+mostrar_contenido(variable)
