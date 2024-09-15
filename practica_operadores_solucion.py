dda8aed96a8d23f2e2be49531344ffcdd67ac28d
CoolCatMoon <coronado.sahid@gmail.com>
2024-09-14 22:22:02 -0600

    practica_operadores_solucion.py

---
+++ practica_operadores_solucion.py
@@ -0,0 +1,69 @@
+# Parte 1: Operadores aritméticos
+a = 7
+b = 3
+
+# Suma
+suma = a + b
+print("Suma:", suma)
+
+# Resta
+resta = a - b
+print("Resta:", resta)
+
+# Multiplicación
+multiplicacion = a * b
+print("Multiplicación:", multiplicacion)
+
+# División
+division = a / b
+print("División:", division)
+
+# Potencia
+potencia = a ** b
+print("Potencia:", potencia)
+
+# División entera
+division_entera = a // b
+print("División entera:", division_entera)
+
+# Módulo
+modulo = a % b
+print("Módulo:", modulo)
+
+
+# Parte 2: Operadores de comparación
+x = 15
+y = 10
+
+# Igualdad
+igual = x == y
+print("¿x es igual a y?:", igual)
+
+# Diferencia
+diferente = x != y
+print("¿x es diferente a y?:", diferente)
+
+# Mayor que
+mayor_que = x > y
+print("¿x es mayor que y?:", mayor_que)
+
+# Menor o igual que
+menor_igual = x <= y
+print("¿x es menor o igual que y?:", menor_igual)
+
+
+# Parte 3: Operadores lógicos
+verdadero = True
+falso = False
+
+# AND
+and_resultado = verdadero and falso
+print("True AND False:", and_resultado)
+
+# OR
+or_resultado = verdadero or falso
+print("True OR False:", or_resultado)
+
+# NOT
+not_resultado = not verdadero
+print("NOT True:", not_resultado)
